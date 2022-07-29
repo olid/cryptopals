8be8141721e0b00219f192b6bfddc0b634761f5d
olid <widosm@widosm.com>
2022-07-29 21:50:33 +0100

    AES EBC mode encryption working

--- CryptoPals/Encryptors/Aes/Aes.swift
+++ CryptoPals/Encryptors/Aes/Aes.swift
@@ -9,15 +9,13 @@ import Foundation
 
 
 struct Aes {
-    static func encrypt(input: ByteArray, key: ByteArray) -> ByteArray {
+    static func decrypt(input: ByteArray, key: ByteArray) -> ByteArray {
         let state = AesState(byteArray: input)
         let keys = RoundKey(byteArray: key).buildKeys().map { key in key.toState }
         
-        let round0State = state.add(key: keys[0])
-        
-        let done = round0State.applyRound(key: keys[1], roundNumber: 1)
-                
-        return []
+        return (1...10)
+            .reduce(state.add(key: keys[0])) { state, round in state.applyRound(key: keys[round], roundNumber: round) }
+            .bytes
     }
 }
 
